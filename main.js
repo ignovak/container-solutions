69ad211e45bfb655660b93ccc170cb3a3b59805d
Igor Novak <igorn@backbase.com>
2016-09-28 18:45:38 +0200

    Group transformations; add links

--- main.js
+++ main.js
@@ -18,10 +18,25 @@ d3.json('data.json', function (error, data) {
   });
 
 function render (data) {
+
+  var links = main.selectAll('line')
+    .data(data.slice(1).map((_, i) => ({ source: data[0], target: data[i + 1] })))
+    .enter()
+    .append('line')
+    .attr('stroke', '#0af')
+
   var agents = main.selectAll('g')
     .data(data)
     .enter()
     .append('g')
+      .attr('transform', function (_, i) {
+        var edgeRadius = outerRadius * (i % 2 ? 0.8 : 1)
+        var offsetX = agentRadius * 2 + (i % 2 ? 60 : 0)
+        var offsetY = agentRadius * 3 + (i % 2 ? -40 : 0)
+        var x = edgeRadius * (1 - Math.cos(angleOffset + angleDelta * i)) + offsetX
+        var y = edgeRadius * Math.sin(angleOffset + angleDelta * i) + offsetY
+        return `translate(${x}, ${y})`
+      })
     .on('mouseenter', function () {
       d3.select(this)
         .select('circle')
@@ -35,19 +50,8 @@ function render (data) {
         .attr('r', agentRadius);
     })
 
-
   agents
     .append('circle')
-      .attr('cx', function (_, i) {
-        var edgeRadius = outerRadius * (i % 2 ? 0.8 : 1)
-        var offset = agentRadius * 2 + (i % 2 ? 60 : 0)
-        return edgeRadius * (1 - Math.cos(angleOffset + angleDelta * i)) + offset
-      })
-      .attr('cy', function (_, i) {
-        var edgeRadius = outerRadius * (i % 2 ? 0.8 : 1)
-        var offset = agentRadius * 3 + (i % 2 ? -40 : 0)
-        return edgeRadius * Math.sin(angleOffset + angleDelta * i) + offset
-      })
       .attr('r', agentRadius)
       .attr('fill', '#fff')
       .attr('stroke', '#0af')
@@ -56,29 +60,21 @@ function render (data) {
     .append('text')
     .text(_ => _.name)
       .attr('text-anchor', 'middle')
-      .attr('x', function (_, i) {
-        var edgeRadius = outerRadius * (i % 2 ? 0.8 : 1)
-        var offset = agentRadius * 2 + (i % 2 ? 60 : 0)
-        return edgeRadius * (1 - Math.cos(angleOffset + angleDelta * i)) + offset
-      })
-      .attr('y', function (_, i) {
-        var edgeRadius = outerRadius * (i % 2 ? 0.8 : 1)
-        var offset = agentRadius * 3 + (i % 2 ? -40 : 0)
-        return edgeRadius * Math.sin(angleOffset + angleDelta * i) + offset
-      })
 
-  simulation = d3.forceSimulation(data)
-    // .force('collide', d3.forceCollide(20))
+  var simulation = d3.forceSimulation(data)
+    // .force('collide', d3.forceCollide(50))
     .force('charge', d3.forceManyBody())
     .force('center', d3.forceCenter(300, 300))
-    // .force('link', d3.forceLink(links).distance(20).strength(1))
+    .force('link', d3.forceLink(links).distance(150).strength(0.1))
     // .force('x', d3.forceX())
     // .force('y', d3.forceY())
     .on('tick', function() {
-      return
-      agents.selectAll('circle')
-        .attr('cx', _ => _.x)
-        .attr('cy', _ => _.y)
+      agents.attr('transform', _ => `translate(${_.x}, ${_.y})`)
+      links
+        .attr('x1', _ => _.source.x)
+        .attr('y1', _ => _.source.y)
+        .attr('x2', _ => _.target.x)
+        .attr('y2', _ => _.target.y)
     })
 
 }
